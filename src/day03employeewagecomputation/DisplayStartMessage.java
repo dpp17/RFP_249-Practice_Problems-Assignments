f1891cf461e09a64699c51910beccea18f27b641
dpp17 <durga123q.com>
2023-01-15 17:16:55 +0530

    Added: Display_Welcome_Message_START

---
+++ src/day03employeewagecomputation/DisplayStartMessage.java
@@ -0,0 +1,10 @@
+package day03employeewagecomputation;
+
+public class DisplayStartMessage {
+
+	public static void main(String[] args) {
+		System.out.println(" : WLECOME TO EMPLOYEE WAGE COMPUTATION PROGRAM : ");
+
+	}
+
+}
