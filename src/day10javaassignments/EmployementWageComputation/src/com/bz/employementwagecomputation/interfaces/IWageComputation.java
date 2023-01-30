73d2655271fa03ce5c68bb79c8f0d0dbd23b70d9
dpp17 <durga123q.com>
2023-01-30 22:48:17 +0530

    Added: EmployementWage Program

---
+++ src/day10javaassignments/EmployementWageComputation/src/com/bz/employementwagecomputation/interfaces/IWageComputation.java
@@ -0,0 +1,13 @@
+package com.bz.employementwagecomputation.interfaces;
+
+
+public interface IWageComputation {
+
+	 void displayMessage();
+	 int employeesPresentFullTime();
+	 int dailyFullTimeEmployeeWage(int wagePerHour, int fullDayHour, int numberOfEmployees);
+	 int employeesPresentPartTime();
+	 int dailyPartTimeEmployeeWage(int wagePerHour, int PartTimeHour, int numberOfEmployees);
+	 int wageForMonth(int workingDays, int wagePerHour, int TimeHour);
+	 void workingHoursOrDaysReachFirst(int timeHour);
+}
