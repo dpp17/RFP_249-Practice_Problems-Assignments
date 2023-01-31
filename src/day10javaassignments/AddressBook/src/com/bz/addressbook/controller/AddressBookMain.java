bb4290caf688effe07327d95fc58566df77c3315
dpp17 <durga123q.com>
2023-01-31 08:09:00 +0530

    Added: path edited

--- src/day10javaassignments/AddressBook/src/com/bz/addressbook/controller/AddressBookMain.java
+++ src/day10javaassignments/AddressBook/src/com/bz/addressbook/controller/AddressBookMain.java
@@ -1,4 +1,4 @@
-package com.bz.addressbook.controller;
+package day10javaassignments.AddressBook.src.com.bz.addressbook.controller;
 import java.util.Scanner;
 
 import com.bz.addressbook.interfaces.InterfaceAddressBook;
