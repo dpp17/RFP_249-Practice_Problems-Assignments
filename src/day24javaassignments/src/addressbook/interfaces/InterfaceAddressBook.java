6058aecbae794647ea7d25d874058ab789d107ee
dpp17 <durga123q.com>
2023-02-20 19:28:13 +0530

    Added: Stream API

---
+++ src/day24javaassignments/src/addressbook/interfaces/InterfaceAddressBook.java
@@ -0,0 +1,11 @@
+package day24javaassignments.src.addressbook.interfaces;
+
+import addressbook.model.Contacts;
+
+public interface InterfaceAddressBook {
+
+	void welcomeDisplay();
+	int createContactDetails(Contacts contacts);
+	void editByName(String firstName);
+	void deleteByFirstName(String firstname);
+}
---
+++ src/day24javaassignments/src/addressbook/services/ImplementationsOfAddressBook.java
@@ -0,0 +1,122 @@
+package day24javaassignments.src.addressbook.services;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import addressbook.interfaces.InterfaceAddressBook;
+import addressbook.model.Contacts;
+
+public class ImplementationsOfAddressBook implements InterfaceAddressBook{
+
+	private static Scanner getDetailInput = new Scanner(System.in);
+	ArrayList<Contacts> storeContactDelails = new ArrayList<Contacts>();
+	
+	public void welcomeDisplay() {
+		System.err.println("===================================");
+		System.out.println("  ::  Welcome To Address_Book  ::  ");
+		System.err.println("==================================="+'\n');
+		
+	}
+
+	public int createContactDetails(Contacts contacts) {
+		System.out.println(" List of All the Contscts ");
+		storeContactDelails.add(contacts);
+		return storeContactDelails.indexOf(contacts);
+	}
+	
+
+	public void editByName(String firstName) {
+		storeContactDelails.stream().filter(name->name.getFirstName().equalsIgnoreCase(firstName)).forEach(name->editContactDetails(name));
+	}
+	
+	//Edit-Update details
+		private void editContactDetails( Contacts contacts) {
+		Contacts con = new Contacts();
+		
+		byte loopExit = 1; 
+			do {
+				System.out.println("Choose Option to Update :: " + '\n' + " 1.FirstName " + '\n' + " 2.LastName " + '\n' + " 3.Address "
+						+ '\n' + " 4.City " + '\n' + " 5.State " + '\n' + " 6.Zip " + '\n'
+								+ " 7.PhoneNumber " + '\n' + " 8.Aadhar_Number " + '\n' + " 9.Email_ID ");
+				byte option = getDetailInput.nextByte();
+				
+				switch(option) {
+				case 1:
+						System.out.println(" FirstName :: "); 
+						contacts.setFirstName(getDetailInput.next());
+					break;
+				case 2:
+						System.out.println(" LastName :: ");
+						contacts.setLastName(getDetailInput.next());
+					break;
+				case 3:
+						System.out.println(" Address :: ");
+						con.setAddress(getDetailInput.next());
+					break;
+				case 4:
+						System.out.println(" City :: ");
+						contacts.setCity(getDetailInput.next());
+					break;
+				case 5:
+						System.out.println(" State :: ");
+						contacts.setState(getDetailInput.next());
+					break;
+				case 6:
+						System.out.println(" Zip :: ");
+						contacts.setZip(getDetailInput.nextInt());
+					break;
+				case 7:
+						System.out.println(" PhoneNumber :: ");
+						contacts.setPhoneNumber(getDetailInput.nextLong());
+					break;
+				case 8:
+						System.out.println(" AadharNumber :: ");
+						con.setAadharCardNumber(getDetailInput.nextLong());
+					break;
+				case 9:
+						System.out.println(" Email_ID :: ");
+						contacts.setEmail(getDetailInput.next());
+					break;
+				default:
+					System.out.println(" ****---- Invalid Input ----**** ");
+				}
+				System.out.println(" Press 1: If you want to update anything else " + '\n' + " Press 0: Exit to Menu ");
+				loopExit = getDetailInput.nextByte();
+			}while(loopExit != 0);
+		}
+		
+		private void contactDetailsPrint( Contacts contacts) {
+			if(contacts != null) {
+				System.out.println(" *************** : Contact Details : *************** ");
+				System.out.println(" FirstName :: " + contacts.getFirstName());
+				System.out.println(" LastName :: " + contacts.getLastName());
+				System.out.println(" Address :: " + contacts.getAddress());
+				System.out.println(" City :: " + contacts.getCity());
+				System.out.println(" State :: " + contacts.getState());
+				System.out.println(" Zip :: " + contacts.getZip());
+				System.out.println(" PhoneNumber :: " + contacts.getPhoneNumber());
+				System.out.println(" AadharCard Number :: " + contacts.getAadharCardNumber());
+				System.out.println(" Email ID:: " + contacts.getEmail());
+				System.out.println("");
+			}
+		}
+
+		public void deleteByFirstName(String firstname) {
+			
+			storeContactDelails.removeAll(storeContactDelails.stream().filter(name->name.getFirstName().equalsIgnoreCase(firstname)).collect(Collectors.toList()));	
+		}
+		
+		private void showAllContacts() {
+			System.out.println("*************** : All Available Contacts- : ***************");
+			for(int i = 0; i < storeContactDelails.size(); i++) {
+				Contacts contacts = storeContactDelails.get(i);
+				if(null != storeContactDelails.get(i)) {
+				System.out.println(" Serial No.:: ABRN_ "+ i );
+				contactDetailsPrint(contacts);
+				}	
+			}
+		}
+		
+		
+}
