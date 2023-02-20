42eb1007d49c19078e140cbd30f1855ef2352e03
dpp17 <durga123q.com>
2023-02-20 20:59:35 +0530

    Added: changes

--- src/day23javaassignments/userregistration/interfaces/IUserRegistration.java
+++ src/day23javaassignments/userregistration/interfaces/IUserRegistration.java
@@ -1,8 +1,8 @@
 package day23javaassignments.userregistration.interfaces;
 
 import day21javaassignments.userregistration.model.UserRegistrationContainer;
-
+@FunctionalInterface
 public interface IUserRegistration {
-	void welcomeDisplay();
+	//void welcomeDisplay();
 	int addDetails(UserRegistrationContainer user);
 }
